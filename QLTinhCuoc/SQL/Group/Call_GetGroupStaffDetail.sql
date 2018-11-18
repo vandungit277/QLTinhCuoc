0cc195a4954ff14eaff19ee2ad45db56a00d2567
Dung <vandungit277@gmail.com>
2018-11-18 11:20:16 +0700

    new file
    new file

---
+++ QLTinhCuoc/SQL/Group/Call_GetGroupStaffDetail.sql
@@ -0,0 +1,50 @@
+﻿USE PowerCall
+GO
+/*
+Author: Dungdv24
+Desc: 18/11/2018
+Desc: Thông tin User có trong những Group nào
+*/
+ALTER PROCEDURE Call_GetGroupStaffDetail
+    @Search INT , --0 Theo tất cả,1 Theo MenuRootID
+    @GroupsID VARCHAR(10) ,
+    @UserName VARCHAR(30)
+AS
+    BEGIN
+        DECLARE @StaffID VARCHAR(10);
+        SELECT  @StaffID = StaffID
+        FROM    dbo.Staff(NOLOCK)
+        WHERE   UserName = @UserName;
+
+        DECLARE @qr NVARCHAR(200) = ' And 1 = 1 '
+        IF @Search = 1 --Người dùng
+            BEGIN
+                SET @qr = N' AND G.GroupsID = ' + @GroupsID
+            END
+        DECLARE @query NVARCHAR(MAX) = N'
+SELECT  g.GroupsID, '+@StaffID+' AS StaffID,g.GroupsName,ISNULL(GS.Status,0) AS [Status]
+FROM    dbo.Groups AS G
+		LEFT JOIN (SELECT * FROM dbo.GroupStaffDetail(NOLOCK) WHERE StaffID= '
+            + @StaffID + ' )  AS GS ON GS.GroupsID = G.GroupsID
+WHERE   G.Status = 1
+     ' + @qr
+        PRINT @query
+        IF EXISTS ( SELECT  1
+                    FROM    dbo.Staff(NOLOCK)
+                    WHERE   UserName = @UserName )
+            BEGIN
+
+                EXEC sp_executesql @query 
+            END
+        ELSE
+            BEGIN
+                SELECT  @query = @query + N' And 5 =4';
+                EXEC sp_executesql @query 
+            END
+    END
+
+/*
+EXEC dbo.Call_GetGroupStaffDetail @Search = 0, -- int
+    @GroupsID = 1 ,-- int
+	@UserName ='thanhtt'
+*/
---
+++ QLTinhCuoc/SQL/Group/Call_IUGroupStaffDetail.sql
@@ -0,0 +1,85 @@
+﻿USE PowerCall
+GO
+/*
+Author: Dungdv24
+Desc: 11/11/2018
+Desc: Danh sách Groups mà User Tham gia
+*/
+ALTER PROCEDURE Call_IUGroupStaffDetail
+    @GroupsID INT ,
+    @Status INT ,
+    @UpdateBy VARCHAR(30) ,
+    @StaffID INT ,
+    @Description NVARCHAR(500)
+AS
+    BEGIN
+
+        IF ISNULL(@Description, '') = ''
+            BEGIN
+                IF @Status = 0
+                    BEGIN
+                        SET @Description = N'OFF'
+                    END
+                ELSE
+                    BEGIN
+                        SET @Description = N'Kích hoạt'
+                    END
+            END
+        IF NOT EXISTS ( SELECT  *
+                        FROM    dbo.Staff(NOLOCK)
+                        WHERE   StaffID = @StaffID )
+            BEGIN
+                SELECT  -1 AS [Code] ,
+                        N'User không tồn tại trên hệ thống' AS [Desc];
+                RETURN;
+            END
+		
+
+        IF EXISTS ( SELECT  1
+                    FROM    dbo.GroupStaffDetail(NOLOCK)
+                    WHERE   StaffID = @StaffID
+                            AND GroupsID = @GroupsID )
+            BEGIN
+                UPDATE  dbo.GroupStaffDetail
+                SET     Status = @Status ,
+                        Description = @Description ,
+                        UpdateBy = @UpdateBy
+                WHERE   StaffID = @StaffID
+                        AND GroupsID = @GroupsID
+                        
+            END
+        ELSE
+            BEGIN
+                INSERT  INTO dbo.GroupStaffDetail
+                        ( StaffID ,
+                          GroupsID ,
+                          Status ,
+                          CreateDate ,
+                          Description ,
+                          UpdateBy
+                        )
+                VALUES  ( @StaffID , -- StaffID - int
+                          @GroupsID , -- GroupsID - int
+                          @Status , -- Status - int
+                          GETDATE() , -- CreateDate - datetime
+                          @Description ,  -- Description - nvarchar(500)
+                          @UpdateBy
+                        )
+            END
+        IF @@ROWCOUNT > 0
+            BEGIN
+                SELECT  1 AS [Code] ,
+                        N'Cập nhật thành công' AS [Desc];
+            END
+        ELSE
+            BEGIN
+                SELECT  0 AS [Code] ,
+                        N'Cập nhật thất bại' AS [Desc];
+            END
+
+
+    END
+
+
+
+	--SELECT * FROM dbo.GroupStaffDetail
